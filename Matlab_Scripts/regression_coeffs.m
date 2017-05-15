b2f20381a2849626c87baa073e753d8aa141d7ca
Jamie Brynes <jamiebrynes7@gmail.com>
2017-05-15 11:33:52 +0100

    chg: Close to final version

--- Matlab_Scripts/regression_coeffs.m
+++ Matlab_Scripts/regression_coeffs.m
@@ -1,11 +1,11 @@
-coeffs = [0.5707, -2.6377];
-coeffs = [-2.6377, -0.5707/-2.6377];
-x = -10:0.01:10;
+coeffs = [-8.1804, -0.8321/-8.1804];
+x = -1:0.01:1;
 y = sigmf(x,coeffs);
 plot(x,y)
 y_fitted = zeros(1,length(y));
 y_fitted(find(y(:) >= 0.5)) = 1;
 y_fitted(find(y(:) < 0.5)) = 0;
+[c index] = min(abs(y-0.5))
 hold on
 plot(x,y_fitted)
 title('Logistic Regression')
